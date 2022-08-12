e8fd7158f4aa1c9259afa71ed336d7df27e81cb3
Gustavo Torres <betogus2009@gmail.com>
2022-08-11 21:13:23 -0300

    commit

--- src/App.js
+++ src/App.js
@@ -9,6 +9,8 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <p>Gustavo Torres</p>
+        <p> Mi e-commerce</p>
         <a
           className="App-link"
           href="https://reactjs.org"
